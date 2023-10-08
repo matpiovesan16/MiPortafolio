a042a69aacc96bde2759d3f4bf291f5eca2c92f0
Matias Piovesan <piovesanmatias@gmail.com>
2023-10-08 11:45:53 -0300

    correccion de error de position navBar

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var menuCerrar = document.getElementById('menuCerrar');
 
 function menuAbrirPortafolio(){
     menu.setAttribute('class', 'fa-solid fa-bars text-3xl text-slate-200 sm:hidden cursor-pointer hover:text-opacity-50 animate__animated animate__fadeInRight hidden');
-    cajaMenu.setAttribute('class', 'flex justify-between w-full p-9 max-sm:fixed max-sm:top-0 max-sm:bg-slate-900 max-sm:z-10 max-sm:h-2/4');
-    navBar.setAttribute('class','text-slate-200 text-xl animate__animated animate__fadeInDown max-sm:flex max-sm:h-[200px] max-sm:flex-col max-sm:items-center max-sm:justify-center relative top-[15%] left-[-5%]');
+    cajaMenu.setAttribute('class', 'flex justify-between w-full absolute p-9 max-sm:fixed max-sm:top-0 max-sm:bg-slate-900 max-sm:z-10 max-sm:h-2/4');
+    navBar.setAttribute('class','text-slate-200 text-xl animate__animated animate__fadeInDown max-sm:flex max-sm:h-[220px] max-sm:flex-col max-sm:items-center max-sm:justify-center relative top-[15%] left-[-5%]');
     menuCerrar.setAttribute('class', 'fa-solid fa-x text-3xl text-slate-200 sm:hidden cursor-pointer hover:text-opacity-50 animate__animated animate__fadeInRight absolute left-[84.5%]');
 
 }
@@ -15,7 +15,7 @@ function menuAbrirPortafolio(){
 
 function menuCerrarPortafolio (){
     menu.setAttribute('class', 'fa-solid fa-bars text-3xl text-slate-200 sm:hidden cursor-pointer hover:text-opacity-50 animate__animated animate__fadeInRight');
-    cajaMenu.setAttribute('class', 'flex justify-between w-full p-9 max-sm:fixed max-sm:top-0 max-sm:bg-slate-900 max-sm:z-10');
+    cajaMenu.setAttribute('class', 'flex justify-between w-full p-9 absolute max-sm:fixed max-sm:top-0 max-sm:bg-slate-900 max-sm:z-10');
     navBar.setAttribute('class','text-slate-200 text-xl animate__animated animate__fadeInDown max-sm:flex max-sm:h-full max-sm:flex-col max-sm:justify-center max-sm:hidden');
     menuCerrar.setAttribute('class', 'fa-solid fa-x text-3xl text-slate-200 sm:hidden cursor-pointer hover:text-opacity-50 animate__animated animate__fadeInRight absolute left-[100%] hidden');
 
